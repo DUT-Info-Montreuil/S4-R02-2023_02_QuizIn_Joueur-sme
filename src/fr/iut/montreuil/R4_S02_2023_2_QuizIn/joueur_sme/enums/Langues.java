0905d4c7b45162e36e4429400d74067c5c66c6ea
rsk94zoo <101595296+rsk94zoo@users.noreply.github.com>
2023-03-16 03:05:06 +0100

    Update classe Langues enum avec l'ajout des champs et ses attributs

--- src/fr/iut/montreuil/R4_S02_2023_2_QuizIn/joueur_sme/enums/Langues.java
+++ src/fr/iut/montreuil/R4_S02_2023_2_QuizIn/joueur_sme/enums/Langues.java
@@ -1,5 +1,20 @@
 package fr.iut.montreuil.R4_S02_2023_2_QuizIn.joueur_sme.enums;
 
 public enum Langues {
-    Francais, Anglais, Allemand, Espagnol, Italien
+    Francais(1, "FR", "Française"),
+    Anglais(2, "EN", "Anglaise"),
+    Allemand(3, "DE", "Allemande"),
+    Espagnole(4, "ES", "Espagnole"),
+    Italien(5, "IT", "Italienne");
+
+    Langues(int id, String code, String label) {
+        this.id = id;
+        this.code = code;
+        this.label = label;
+    }
+
+    private final int id;
+    private final String code;
+    private final String label;
+
 }
